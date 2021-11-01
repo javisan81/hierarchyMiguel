7c3dc58d77073b7d9f00c1ef41f8a2ca014dead8
javierlopez <javierlopez.tw@packlink.com>
2021-11-01 20:09:30 +0100

    first commit for hierachy kata, it creates the project

---
+++ src/test/java/com/packlink/hierachy/infrastructure/DummyTest.java
@@ -0,0 +1,13 @@
+package com.packlink.hierachy.infrastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        assertThat(true, equalTo(true));
+    }
+}
